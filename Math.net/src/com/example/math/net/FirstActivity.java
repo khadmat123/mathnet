70dea9743a30ee56a58c620230ee6a45b41d0096
Matthew <khadmat96@gmail.com>
2014-03-10 00:29:13 +0600

    -Начальные вопросы -Новая активити(продолжение начальных вопросов) -Убрана логин активити

--- Math.net/src/com/example/math/net/FirstActivity.java
+++ Math.net/src/com/example/math/net/FirstActivity.java
@@ -2,22 +2,32 @@ package com.example.math.net;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class FirstActivity extends Activity {
-	TextView textView2;
+	 public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	TextView textView1;
+	Button buttont;
+	EditText EditText1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_first);
-		textView2 = (TextView) findViewById(R.id.textView2);
+		textView1 = (TextView) findViewById(R.id.textView1);
+		buttont =(Button) findViewById(R.id.button1);
+		EditText1= (EditText) findViewById(R.id.editText1);
 	}
-	
+	int a = 0;
+	static int c = 0;
+	int b = 0;
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		  menu.add("menu1");
@@ -34,7 +44,51 @@ public class FirstActivity extends Activity {
     }
 	public void go (View view)
 	{
-		textView2.setText("Нажали го");
+		if (a==0)
+		{
+			buttont.setText("Дальше");
+			textView1.setText("2+2*2=?");
+			EditText1.setText("");
+		}
+		if (a==1)
+		{
+			EditText editText = (EditText) findViewById(R.id.editText1);
+			String answer = editText.getText().toString();
+			b=Integer.parseInt(answer);
+			if (b==6)
+			{
+				c++;
+			}
+			textView1.setText("2^9=?");
+			EditText1.setText("");
+		}
+		if (a==2)
+		{
+			EditText editText = (EditText) findViewById(R.id.editText1);
+			String answer = editText.getText().toString();
+			b=Integer.parseInt(answer);
+			if (b==512)
+			{
+				c++;
+			}
+			textView1.setText("x^3=1; x-?");
+			EditText1.setText("");
+		}
+		if (a==3)
+		{
+			EditText editText = (EditText) findViewById(R.id.editText1);
+			String answer = editText.getText().toString();
+			b=Integer.parseInt(answer);
+			if (b==1)
+			{
+				c++;
+			}
+			textView1.setText("");
+			Intent intent = new Intent(this, SecondActivity.class);
+		    intent.putExtra(EXTRA_MESSAGE, Integer.toString(c));
+		    startActivity(intent);
+		}
+		a++;
 	}
 
 }
--- Math.net/src/com/example/math/net/SecondActivity.java
+++ Math.net/src/com/example/math/net/SecondActivity.java
@@ -1,31 +1,31 @@
 package com.example.math.net;
 
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
-
-public class MainActivity extends Activity {
-
+public class SecondActivity extends Activity {
+	TextView textViewSecond;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_second);
+		textViewSecond = (TextView) findViewById(R.id.textViewSecond);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.second, menu);
 		return true;
 	}
-	public void sendMessage (View view)
-    {
-    	Intent intent = new Intent(this, FirstActivity.class);
-    	startActivity(intent);
-    }
-
+	
+	int c=FirstActivity.c;
+	public void go2 (View view)
+	{
+	textViewSecond.setText("её значение "+c);
+	}
 }
